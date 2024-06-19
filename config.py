25d6588fd51c3cb76ffec4e05537fbd73313222d
XiaojingReady <614333484@qq.com>
2024-06-19 16:02:04 +0800

    Merge remote-tracking branch 'origin/master'

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ from PIL import ImageFile
 
 class Config():
     def __init__(self,):
-        os.environ["CUDA_VISIBLE_DEVICES"] = "2, 3"
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
         self.backbone_name = 'resnet50'                         # resnet50 resnet18
         self.freeze_feature_extractor_weights = False           # True False
         self.flag_if_poly_adjust_lr = False                     # True False
